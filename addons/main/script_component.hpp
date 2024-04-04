ddb4c651d2fb59208891e25bf76e00d013a68d22
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-04-04 16:20:28 -0500

    fixed paths and .gitignore

--- addons/main/script_component.hpp
+++ addons/main/script_component.hpp
@@ -1,4 +1,4 @@
 #define COMPONENT main
 #define COMPONENT_BEAUTIFIED Main
-#include "\z\test\addons\main\script_mod.hpp"
-#include "\z\test\addons\main\script_macros.hpp"
+#include "\z\lmd_tst\addons\main\script_mod.hpp"
+#include "\z\lmd_tst\addons\main\script_macros.hpp"
